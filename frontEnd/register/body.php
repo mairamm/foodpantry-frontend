10cbab1701fec2bc1625c27f0b460277e0589c1b
Loay Samha <lsamha@csub.edu>
2021-02-19 03:33:52 -0800

    changes to header and footer

--- frontEnd/register/body.php
+++ frontEnd/register/body.php
@@ -12,15 +12,26 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/css/pikaday.min.css">
     <link rel="stylesheet" href="assets/css/select.css">
 
+    <link rel="stylesheet" href="Footer-Clean.css"/>
+    <link rel="stylesheet" href="Header-Blue.css"/>
+    <link rel="stylesheet" href="Header-Dark.css"/>
+    <link rel="stylesheet" href="Lightbox-Gallery.css"/>
+    <link rel="stylesheet" href="Navigation-with-Buttons.css"/>
+    <link rel="stylesheet" href="Simple-Slider.css"/>
+    <link rel="stylesheet" href="Carousel.css"/>
+    <link rel="stylesheet" href="Footer.css"/>
+    <link rel="stylesheet" href="Nav-Bar.css"/>
+    <link rel="stylesheet" href="Buttons.css"/>
+    <link rel="stylesheet" href="styles.css"/>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
  
 
 </head>
 
 <body>
-	//start of header
     <div>
-        <div class="alert alert-success" role="alert" style="background: #ffc629;margin: 0px 0px 1px;"><span style="text-align: center;color: rgb(0,0,0);font-size: 20px;"><strong>Alerts:</strong><a class="alert-link" href="#" style="color: rgb(0,0,0);">Covid-19<strong>|</strong></a><a class="alert-link" href="#" style="color: rgb(0,0,0);">Return to Campus<strong>|</strong></a><a class="alert-link" href="#" style="color: rgb(0,0,0);">CARES Act</a></span></div>
+    <div class="alert alert-success" role="alert" style="background: #ffc629;margin: 0px 0px 1px;"><span style="text-align: center;color: rgb(0,0,0);font-size: 20px;text-align: center;"><strong><center>Alerts:</strong><a class="alert-link" href="#" style="color: rgb(0,0,0);">Covid-19<strong>|</strong></a><a class="alert-link" href="#" style="color: rgb(0,0,0);">Return to Campus<strong>|</strong></a><a class="alert-link" href="#" style="color: rgb(0,0,0);">CARES Act</center></a></span></div>
     </div>
     <nav class="navbar navbar-lit navbar-expand-md navigation-clean-button" style="background: #0c1975;">
         <div class="container"><a class="navbar-brand" href="#" style="color: rgb(255,255,255);font-size: 35px;font-style: normal;font-family: Bitter, serif;">Food Pantry</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
@@ -31,11 +42,10 @@
                     <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#" style="color: rgb(255,255,255);">Contact</a>
                         <div class="dropdown-menu"><a class="dropdown-item" href="#">Phone number</a><a class="dropdown-item" href="#">Location</a></div>
                     </li>
-                </ul><span class="navbar-text actions"><a class="btn btn-light action-button" role="button" href="#" style="background: #ffc629;color: rgb(0,0,0);padding: 8px 20px;">Login</a><a class="btn btn-light action-button" role="button" href="#" style="background: #ffc629;color: rgb(0,0,0);">Sign Up</a></span>
+                </ul><span class="navbar-text actions"><a class="btn btn-light action-button" role="button" href="#" style="background: #ffc629;color: rgb(0,0,0);padding: 8px 20px;">Login</a></span>
             </div>
         </div>
 	</nav>
-	//end of header
     <main class="page lanidng-page"></main>
     <section class="portfolio-block website gradient" style="background: url(&quot;assets/img/gray.png&quot;);">
         <section>
@@ -72,7 +82,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/pikaday.min.js"></script>
     <script src="assets/js/theme.js"></script>
 
-	//start of footer
     <div class="footer-clean" style="padding: 0px;">
         <footer>
             <div class="container">
@@ -105,7 +114,6 @@
             </div>
         </footer>
     </div>
-	//end of footer
 
 </body>
 </html>
