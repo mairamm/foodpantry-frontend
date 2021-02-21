a2eba63b8af904f3d2716280f129230bff2ff72a
ArieBattle <abattle@csub.edu>
2021-02-21 10:32:07 -0800

    i had to fix some of the libraries, they were pointing to the wrong directories

--- frontEnd/register/body.php
+++ frontEnd/register/body.php
@@ -5,13 +5,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Home - Brand</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
-    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
-    <link rel="stylesheet" href="assets/css/Application-Form.css">
+    <link rel="stylesheet" href="ionicons.min.css">
+    <link rel="stylesheet" href="Application-Form.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/css/pikaday.min.css">
-    <link rel="stylesheet" href="assets/css/select.css">
-
+    <link rel="stylesheet" href="select.css">
     <link rel="stylesheet" href="Footer-Clean.css"/>
     <link rel="stylesheet" href="Header-Blue.css"/>
     <link rel="stylesheet" href="Header-Dark.css"/>
