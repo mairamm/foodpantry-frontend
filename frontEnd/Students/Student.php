1ab14fabc526cdbe74f3519f00fcdbcde34a780b
ArieBattle <abattle@csub.edu>
2021-02-21 11:31:35 -0800

    updated all the links in the footer part of the page

--- frontEnd/Students/Student.php
+++ frontEnd/Students/Student.php
@@ -73,22 +73,24 @@
 		    <div class="col-sm-4 col-md-3 item">
 			<ul>
 			    <li>&nbsp;</li>
-			    <li style="color: rgb(255,255,255);">Privacy</li>
-			    <li style="color: rgb(255,255,255);">Politics</li>
-			    <li style="color: rgb(255,255,255);">Accessibility</li>
-			    <li style="color: rgb(255,255,255);">Maps</li>
-			    <li style="color: rgb(255,255,255);">FAQs</li>
+			    <li style="color: rgb(255,255,255);"><a href="https://www.csub.edu/privacy/">Privacy</a></li>
+			    <li style="color: rgb(255,255,255);"><a href="https://policies.csub.edu/">Policies</a></li>
+			    <li style="color: rgb(255,255,255);"><a href="https://www.csub.edu/equity-inclusion-compliance/ada">Accessibility</a></li>
+			    <li style="color: rgb(255,255,255);"><a href="https://www.csub.edu/campusmap/">Maps</a></li>
+
+
 			</ul>
 		    </div>
 		    <div class="col-sm-4 col-md-3 item">
 			<ul>
 			    <li></li>
 			    <li>&nbsp;</li>
-			    <li style="color: rgb(249,249,249);">University</li>
-			    <li style="color: rgb(255,255,255);">Emergency Info</li>
-			    <li style="color: rgb(255,255,255);">Safety Plan</li>
-			    <li style="color: rgb(255,255,255);">Return to Campus</li>
-			</ul>
+			    <li style="color: rgb(249,249,249);"><a href="https://www.csub.edu/bas/police/index.html">University Police</a></li>
+			    <li style="color: rgb(255,255,255);"><a href="https://www.csub.edu/">University</a></li>
+			    <li style="color: rgb(255,255,255);"><a href="https://www.csub.edu/bas/policy/CampusSafetyPlan.pdf">Safety Plan</a></li>
+			
+			    <li style="color: rgb(255,255,255);"><a href="https://www.csub.edu/sustainability/foodpantry/FAQ_FoodPantry/index.html">FAQs</a></li>
+</ul>
 		    </div>
 		    <div class="col-1 col-sm-4 col-md-3 offset-md-0 item">
 			<ul></ul><small style="color: rgb(255,255,255);"><br>California State University, Bakersfield 9001 Stockdale Highway Bakersfield, CA 93311 (661)-654-CSUB<br></small><img class="img-fluid float-right bounce animated" src="../images/food_pantry_image.jpg" width="100" height="100" style="margin: 5px;padding: 5px;">
