071b99c94bdd80e2e295ab364f4fae073693102d
Maira Mendez <mmendez18@csub.edu>
2021-03-04 21:00:35 -0800

    deleted the css files from homePage since they were moved and changed the way they were linked to the correct path

--- restrucured/homePage/Homepage.php
+++ restrucured/homePage/Homepage.php
@@ -3,17 +3,17 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
     <title>Homepage</title>
-    <link rel="stylesheet" href="Footer-Clean.css"/>
-    <link rel="stylesheet" href="Header-Blue.css"/>
-    <link rel="stylesheet" href="Header-Dark.css"/>
-    <link rel="stylesheet" href="Lightbox-Gallery.css"/>
-    <link rel="stylesheet" href="Navigation-with-Buttons.css"/>
-    <link rel="stylesheet" href="Simple-Slider.css"/>
-    <link rel="stylesheet" href="Carousel.css"/>
-    <link rel="stylesheet" href="Footer.css"/>
-    <link rel="stylesheet" href="Nav-Bar.css"/>
-    <link rel="stylesheet" href="Buttons.css"/>
-    <link rel="stylesheet" href="styles.css"/>	
+    <link rel="stylesheet" href="/assets/css/homePage/Footer-Clean.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Header-Blue.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Header-Dark.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Lightbox-Gallery.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Navigation-with-Buttons.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Simple-Slider.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Carousel.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Footer.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Nav-Bar.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Buttons.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/styles.css"/>	
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
 
