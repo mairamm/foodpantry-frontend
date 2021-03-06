1a8364aa5f9530324c5e5bd0249d2531838bc50a
ArieBattle <abattle@csub.edu>
2021-03-06 15:43:49 -0800

    i had to change some links to the sign up becuase it was pointing to the wrong page

---
+++ restrucured/skel/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+    <title>Homepage</title>
+    <link rel="stylesheet" href="/assets/css/homePage/Footer-Clean.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Header-Blue.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Header-Dark.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Lightbox-Gallery.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Navigation-with-Buttons.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Simple-Slider.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Carousel.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Footer.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Nav-Bar.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/Buttons.css"/>
+    <link rel="stylesheet" href="/assets/css/homePage/styles.css"/>	
+    <link rel="stylesheet" href="/assets/css/lib/bootstrap.min.css"/>	
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
