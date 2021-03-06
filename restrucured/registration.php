6618bf890387231fbce3356fb30ea5b0701840c9
Maira Mendez <mmendez18@csub.edu>
2021-03-06 15:45:55 -0800

    removed header, navbar, and footer from registration.php

--- restrucured/registration.php
+++ restrucured/registration.php
@@ -1,46 +1,8 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Home - Brand</title>
-    <link rel="stylesheet" href="./assets/css/lib/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
-    <link rel="stylesheet" href="./assets/css/registration/ionicons.min.css">
-    <link rel="stylesheet" href="./assets/css/registration/Application-Form.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/css/pikaday.min.css">
-    <link rel="stylesheet" href="./assets/css/registration/select.css">
-    <link rel="stylesheet" href="./assets/css/registration/Footer-Clean.css"/>
-    <link rel="stylesheet" href="./assets/css/registration/Header-Blue.css"/>
-    <link rel="stylesheet" href="./assets/css/registration/Header-Dark.css"/>
-    <link rel="stylesheet" href="./assets/css/registration/Lightbox-Gallery.css"/>
-    <link rel="stylesheet" href="./assets/css/registration/Navigation-with-Buttons.css"/>
-    <link rel="stylesheet" href="./assets/css/registration/Simple-Slider.css"/>
-    <link rel="stylesheet" href="./assets/css/registration/Carousel.css"/>
-    <link rel="stylesheet" href="./assets/css/registration/Footer.css"/>
-    <link rel="stylesheet" href="./assets/css/registration/Nav-Bar.css"/>
-    <link rel="stylesheet" href="./assets/css/registration/Buttons.css"/>
-    <link rel="stylesheet" href="./assets/css/registration/styles.css"/>
-</head>
 
 <body>
-    <div>
-    <div class="alert alert-success" role="alert" style="background: #ffc629;margin: 0px 0px 1px;"><span style="text-align: center;color: rgb(0,0,0);font-size: 20px;text-align: center;"><strong><center>Alerts:</strong><a class="alert-link" href="#" style="color: rgb(0,0,0);">Covid-19<strong>|</strong></a><a class="alert-link" href="#" style="color: rgb(0,0,0);">Return to Campus<strong>|</strong></a><a class="alert-link" href="#" style="color: rgb(0,0,0);">CARES Act</center></a></span></div>
-    </div>
-    <nav class="navbar navbar-lit navbar-expand-md navigation-clean-button" style="background: #0c1975;">
-        <div class="container"><a class="navbar-brand" href="#" style="color: rgb(255,255,255);font-size: 35px;font-style: normal;font-family: Bitter, serif;">Food Pantry</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navcol-1">
-                <ul class="nav navbar-nav mr-auto">
-                    <li class="nav-item"><a class="nav-link active" href="#" style="color: rgb(255,255,255);">Donations</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#" style="color: rgb(255,255,255);">Inventory</a></li>
-                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#" style="color: rgb(255,255,255);">Contact</a>
-                        <div class="dropdown-menu"><a class="dropdown-item" href="#">Phone number</a><a class="dropdown-item" href="#">Location</a></div>
-                    </li>
-                </ul><span class="navbar-text actions"><a class="btn btn-light action-button" role="button" href="#" style="background: #ffc629;color: rgb(0,0,0);padding: 8px 20px;">Login</a></span>
-            </div>
-        </div>
-	</nav>
     <main class="page lanidng-page"></main>
     <section class="portfolio-block website gradient" style="background: url(&quot;./assets/img/gray.png&quot;);">
         <section>
@@ -79,39 +41,6 @@
 <br>
 <span class="submit"><a class="submit" role="button" href="#" style="background: #ffc629;color: rgb(0,0,0);padding: 8px 20px;">Submit</a></span>
 <br>
-    <div class="footer-clean" style="padding: 0px;">
-        <footer>
-            <div class="container">
-                <div class="row justify-content-center" style="background: #0c1975;">
-                    <div class="col-sm-4 col-md-3 item">
-                        <ul><center>
-                            <li></li>
-                            <li style="color: rgb(255,255,255);"><a href="https://www.csub.edu/privacy/">Privacy</a></li>
-                            <li style="color: rgb(255,255,255);"><a href="https://policies.csub.edu/">Policies</a></li>
-			<li style="color: rgb(255,255,255);"><a href="https://www.csub.edu/equity-inclusion-compliance/ada">Accessibility</a></li>
-			<li style="color: rgb(255,255,255);"><a href="https://www.csub.edu/campusmap/"></a>Maps</li>
-
-                        </ul>
-                    </div>
-                    <div class="col-sm-4 col-md-3 item">
-                        <ul>
-                            <li></li>
-                            <li></li>
-                            <li style="color: rgb(249,249,249);"><a href="https://www.csub.edu/bas/police/index.html">University Police</a></li>
-                            <li style="color: rgb(255,255,255);"><a href="https://www.csub.edu/">University</a></li>
-			    <li style="color: rgb(255,255,255);"><a href="https://www.csub.edu/bas/policy/CampusSafetyPlan.pdf">Safety Plan</a></li>
-
-                            <li style="color: rgb(255,255,255);"><a href="https://www.csub.edu/sustainability/foodpantry/FAQ_FoodPantry/index.html">FAQS</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-1 col-sm-4 col-md-3 offset-md-0 item">
-                        <ul></ul><small style="color: rgb(255,255,255);"><br/>California State University, Bakersfield 9001 Stockdale Highway Bakersfield, CA 93311 (661)-654-CSUB<br/></small>
-                        <img class="img-fluid float-right" src="./assets/img/foodPantryLogo.jpg" width="100" height="100" style="margin: 5px;padding: 5px;"/>
-                   </center> </div>
-                </div>
-            </div>
-        </footer>
-    </div>
 
 </body>
 </html>
