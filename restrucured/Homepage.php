9bfd16048cdbad8531139708ca6bad3ec580eacc
Maira Mendez <mmendez18@csub.edu>
2021-03-07 03:02:47 -0800

    added the navagation bar to the homepage

--- restrucured/Homepage.php
+++ restrucured/Homepage.php
@@ -1,29 +1,12 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 <?php
-	include('skel/header.php');
+    include('skel/header.php');
+    include('skel/navbarHome.php');
 ?>
 </head>
-
-<body style="color: rgb(0,0,0);font-size: 15px;text-align: center;">
-    <div>
-        <div class="alert alert-success" role="alert" style="background: #ffc629;margin: 0px 0px 1px;"><span style="text-align: center;color: rgb(0,0,0);font-size: 20px;"><strong>Alerts:</strong><a class="alert-link" href="#" style="color: rgb(0,0,0);">Covid-19<strong>|</strong></a><a class="alert-link" href="#" style="color: rgb(0,0,0);">Return to Campus<strong>|</strong></a><a class="alert-link" href="#" style="color: rgb(0,0,0);">CARES Act</a></span></div>
-    </div>
-    <nav class="navbar navbar-lit navbar-expand-md navigation-clean-button" style="background: #0c1975;">
-        <div class="container"><a class="navbar-brand" href="#" style="color: rgb(255,255,255);font-size: 35px;font-style: normal;font-family: Bitter, serif;">Food Pantry</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navcol-1">
-                <ul class="nav navbar-nav mr-auto">
-                    <li class="nav-item"><a class="nav-link active" href="https://www.csub.edu/sustainability/foodpantry/FAQ_FoodPantry/Food%20Pantry%20Donation%20Suggestions.pdf" style="color: rgb(255,255,255);">Donations</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#" style="color: rgb(255,255,255);">Inventory</a></li>
-                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#" style="color: rgb(255,255,255);">Contact</a>
-                        <div class="dropdown-menu"><a class="dropdown-item" href="#">Phone number</a><a class="dropdown-item" href="#">Location</a></div>
-                    </li>
-                </ul><span class="navbar-text actions"><a class="btn btn-light action-button" role="button" href="#" style="background: #ffc629;color: rgb(0,0,0);padding: 8px 20px;">Login</a><a class="btn btn-light action-button" role="button" href="http://delphi.cs.csubak.edu/~foodpantry2/foodpantry-frontend/frontEnd/register/" style="background: #ffc629;color: rgb(0,0,0);">Sign Up</a></span>
-            </div>
-        </div>
-    </nav>
-    <div class="simple-slider">
         <div class="swiper-container">
             <div class="swiper-wrapper"></div>
             <div class="swiper-pagination"></div>
