5b91b7b7ce147687b85ec8edd33e5255cee4d612
Maira Mendez <mmendez18@csub.edu>
2021-03-07 03:27:45 -0800

    created a navation bar file for the staff page and included it in staff.php

---
+++ restrucured/skel/navbarStaff.php
@@ -0,0 +1,7 @@
+<div class="alert alert-success text-center" role="alert" style="background: #ffc629;margin: 0px 0px 1px;"><span style="text-align: center;color: rgb(0,0,0);font-size: 20px;"><strong>Alerts:&nbsp;</strong><a class="alert-link" href="#" style="color: rgb(0,0,0);">Covid-19<strong>&nbsp;|&nbsp;</strong></a><a class="alert-link" href="#" style="color: rgb(0,0,0);">Return to Campus<strong>&nbsp;|&nbsp;</strong></a><a class="alert-link" href="#" style="color: rgb(0,0,0);">CARES Act</a></span></div>
+</div>
+ <nav class="navbar navbar-light navbar-expand-md navigation-clean-button" style="background: #0c1975;">
+    <div class="container"><a class="navbar-brand text-left" href="#" style="color: rgb(255,255,255);font-size: 35px;font-style: normal;font-family: Bitter, serif;">Food Pantry</a>
+        <div class="container"><button class="btn btn-primary ml-auto" type="button">Add</button><button class="btn btn-primary ml-auto" type="button">Update</button><button class="btn btn-primary m-auto" type="button">Delete</button></div><button class="navbar-toggler" data-toggle="collapse"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+    </div>
+ </nav>
