0a76434c9607bc64563276c3d5d39471de222aa0
ArieBattle <abattle@csub.edu>
2021-03-06 16:03:31 -0800

    Revert "repushing had errors"
    This reverts commit 9e5433d17f096d39b2002d0dc3a509b5ab5bb8e0.

--- restrucured/Homepage.php
+++ restrucured/Homepage.php
@@ -31,7 +31,7 @@
                     <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#" style="color: rgb(255,255,255);">Contact</a>
                         <div class="dropdown-menu"><a class="dropdown-item" href="#">Phone number</a><a class="dropdown-item" href="#">Location</a></div>
                     </li>
-                </ul><span class="navbar-text actions"><a class="btn btn-light action-button" role="button" href="#" style="background: #ffc629;color: rgb(0,0,0);padding: 8px 20px;">Login</a><a class="btn btn-light action-button" role="button" href="http://delphi.cs.csubak.edu/~abattle/foodpantry-frontend/restrucured/registration.php" style="background: #ffc629;color: rgb(0,0,0);">Sign Up</a></span>
+                </ul><span class="navbar-text actions"><a class="btn btn-light action-button" role="button" href="#" style="background: #ffc629;color: rgb(0,0,0);padding: 8px 20px;">Login</a><a class="btn btn-light action-button" role="button" href="http://delphi.cs.csubak.edu/~foodpantry2/foodpantry-frontend/frontEnd/register/" style="background: #ffc629;color: rgb(0,0,0);">Sign Up</a></span>
             </div>
         </div>
     </nav>
