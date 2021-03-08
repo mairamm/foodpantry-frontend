bfd4531b6223176dd1330a8e4a46ee20d273de86
Maira Mendez <mmendez18@csub.edu>
2021-03-08 15:20:20 -0800

    deleted broken html

--- restrucured/Homepage.php
+++ restrucured/Homepage.php
@@ -10,29 +10,18 @@
 </head>
 
 
-    <section class="page-section clearfix">
-        <div class="container">
-            <div class="intro"><img class="img-fluid intro-img mb-3 mb-lg-0 rounded" src="./assets/img/comeSign.jpg">
-                <div class="text-center intro-text p-5 rounded bg-faded">
-                    <h2 class="section-heading mb-4"><span class="section-heading-upper">About Us</span><span class="section-heading-lower">Food Pantry</span></h2>
-                    <p class="mb-3"><p style="font-size: 16px;">The CSUB Food Pantry is open to CSUB students, staff, and faculty. 
-                    <br>The Food Pantry is guided by the CSUB Food and Housing Security Committee, the Sustainability Department, and the Executive Director of Associated Students, Inc. The project goals are to increase food security, reduce food waste, and expand nutritional offerings at CSU Bakersfield.
-                    <br>Due to the temporary closure of the CSUB campus, the CSUB Food Pantry will have reduced hours of operation beginning March 18th, 2020.</p></p>
-            </div>
-        </div>
-    </section>
-
-
-<div class="container" style="width: 1000px;">
-    <div class="product-item">
-        <div class="d-flex product-item-title">
-            <div class="d-flex mx-auto bg-faded p-5 rounded" style="opacity: 1;filter: invert(0%);">
-                <h2 class="section-heading mb-0"><span class="section-heading-lower">How does it work?</span></h2>
-            </div>
+<section class="page-section clearfix">
+    <div class="container">
+        <div class="intro"><img class="img-fluid intro-img mb-3 mb-lg-0 rounded" src="./assets/img/comeSign.jpg">
+            <div class="text-center intro-text p-5 rounded bg-faded">
+                <h2 class="section-heading mb-4"><span class="section-heading-upper">About Us</span><span class="section-heading-lower">Food Pantry</span></h2>
+                <p class="mb-3"><p style="font-size: 16px;">The CSUB Food Pantry is open to CSUB students, staff, and faculty. 
+                <br>The Food Pantry is guided by the CSUB Food and Housing Security Committee, the Sustainability Department, and the Executive Director of Associated Students, Inc. The project goals are to increase food security, reduce food waste, and expand nutritional offerings at CSU Bakersfield.
+                <br>Due to the temporary closure of the CSUB campus, the CSUB Food Pantry will have reduced hours of operation beginning March 18th, 2020.</p></p>
         </div>
     </div>
-            <img class="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="./assets/img/points.jpg" />
-</div>
+</section>
+
 
 <?php
     include('skel/footer.php');
