10c1a44d62f246ddaebdedc2bd0d9153e7912e1f
Maira Mendez <mmendez18@csub.edu>
2021-03-08 04:27:04 -0800

    changed the look of the homepage. still under construction.

--- restrucured/Homepage.php
+++ restrucured/Homepage.php
@@ -6,102 +6,33 @@
     include('skel/header.php');
     include('skel/navbarHome.php');
 ?>
+    <link rel="stylesheet" href="./assets/css/lib/bootstrap.min.css">
 </head>
 
-        <div class="swiper-container">
-            <div class="swiper-wrapper"></div>
-            <div class="swiper-pagination"></div>
-            <div class="swiper-button-prev"></div>
-            <div class="swiper-button-next"></div>
-        </div>
-        <div class="carousel slide carousel-fade" data-ride="carousel" data-interval="1000" id="carousel-1">
-            <div class="carousel-inner">
-                <div class="carousel-item active"><img class="w-100 d-block" src="./assets/img/manSpeaking.jpg" alt="Slide Image" width="800" height="533" /></div>
-                <div class="carousel-item">
-                    <p class="lead"><br />&quot;CSUB Food Pantry &amp; Edible Garden: A holistic Approach to Fighting Campus Food Insecurities&quot;<br /><br /></p><img class="w-100 d-block" src="./assets/img/EdibleGardenSignandRaisedBed.jpg" alt="Slide Image" width="800" height="300" />
-                </div>
-                <div class="carousel-item"><img class="w-100 d-block" src="./assets/img/Food pantry map.png" alt="Slide Image" /></div>
-                <div class="carousel-item"><img class="w-100 d-block" src="./assets/img/stockedShelf.jpg" alt="Slide Image" /></div>
-                <div class="carousel-item"><img class="w-100 d-block" src="./assets/img/distribution.webp" alt="Slide Image" /></div>
-            </div>
-            <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-slide="prev"><span class="carousel-control-prev-icon"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-slide="next"><span class="carousel-control-next-icon"></span><span class="sr-only">Next</span></a></div>
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-1" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-1" data-slide-to="1"></li>
-                <li dta-target="#carousel-1" data-slide-to="2"></li>
-                <li data-target="#carousel-1" data-slide-to="3"></li>
-                <li data-target="#carousel-1" data-slide-to="4"></li>
-            </ol>
-        </div>
-    </div>
-    <div>
+
+    <section class="page-section clearfix">
         <div class="container">
-            <div class="row">
-                <div class="col">
-                    <p style="margin: 0px 0px 0px;">How does it work?</p>
-                    <p><br />Each visitor gets one visit per week and up to 16 points per visit. Each item in stock have unique points given<br /></p>
-                    <div class="table-responsive">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>1 POINT</th>
-                                    <th>2 POINT</th>
-                                    <th>3 POINT</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Side Dish or Non-Food Item</td>
-                                    <td>One Meal</td>
-                                    <td>Items Lasting Multiple Meals</td>
-                                </tr>
-                                <tr>
-                                    <td>Granola Bar</td>
-                                    <td>Canned Soup</td>
-                                    <td>Box of Pasta</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="table-responsive">
-                        <table class="table">
-                            <thead>
-                                <tr></tr>
-                            </thead>
-                            <tbody>
-                                <tr></tr>
-                                <tr>
-                                    <td>Canned Fruit</td>
-                                    <td>Chili</td>
-                                    <td>Boxed Cereal</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="table-responsive">
-                        <table class="table">
-                            <thead>
-                                <tr></tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Full-Size Toiletry Item</td>
-                                    <td>Top Ramen</td>
-                                    <td>Dry Beans</td>
-                                </tr>
-                                <tr>
-                                    <td>Up to FOUR 1 point items</td>
-                                    <td>Up to THREE 2 point items</td>
-                                    <td>Up to THREE 3 point items</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+            <div class="intro"><img class="img-fluid intro-img mb-3 mb-lg-0 rounded" src="./assets/img/comeSign.jpg">
+                <div class="text-center intro-text p-5 rounded bg-faded">
+                    <h2 class="section-heading mb-4"><span class="section-heading-upper">About Us</span><span class="section-heading-lower">Food Pantry</span></h2>
+                    <p class="mb-3"><p style="font-size: 16px;">The CSUB Food Pantry is open to CSUB students, staff, and faculty. 
+                    <br>The Food Pantry is guided by the CSUB Food and Housing Security Committee, the Sustainability Department, and the Executive Director of Associated Students, Inc. The project goals are to increase food security, reduce food waste, and expand nutritional offerings at CSU Bakersfield.
+                    <br>Due to the temporary closure of the CSUB campus, the CSUB Food Pantry will have reduced hours of operation beginning March 18th, 2020.</p></p>
+            </div>
+        </div>
+    </section>
+
+
+<div class="container" style="width: 1000px;">
+    <div class="product-item">
+        <div class="d-flex product-item-title">
+            <div class="d-flex mx-auto bg-faded p-5 rounded" style="opacity: 1;filter: invert(0%);">
+                <h2 class="section-heading mb-0"><span class="section-heading-lower">How does it work?</span></h2>
             </div>
         </div>
     </div>
-</body>
+            <img class="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="./assets/img/points.jpg" />
+</div>
 
 <?php
     include('skel/footer.php');
