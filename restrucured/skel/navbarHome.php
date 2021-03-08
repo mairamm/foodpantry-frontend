e617e8b9151a1d838b43e37e8a61676324b36e9e
Maira Mendez <mmendez18@csub.edu>
2021-03-08 02:10:44 -0800

    made the covid alert look like csub's home page alert

--- restrucured/skel/navbarHome.php
+++ restrucured/skel/navbarHome.php
@@ -1,10 +1,14 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Quattrocento+Sans&display=swap" rel="stylesheet">
+
+<link href="/your-path-to-fontawesome/css/all.css" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@310;400;700&display=swap" rel="stylesheet">
 
-<body style="color: rgb(0,0,0);font-size: 15px;text-align: center;">
+<body style="color: rgb(0,0,0);font-size: 17px;text-align: center;">
     <div>
-
-        <div class="alert alert-success" role="alert" style="background: #ffc629;margin: 0px 0px 1px;"><span style="text-align: center;color: rgb(0,0,0);font-size: 20px;"><strong>Alerts:</strong><a class="alert-link" href="#" style="color: rgb(0,0,0);">Covid-19<strong>|</strong></a><a class="alert-link" href="#" style="color: rgb(0,0,0);">Return to Campus<strong>|</strong></a><a class="alert-link" href="#" style="color: rgb(0,0,0);">CARES Act</a></span></div>
+        <div class="alert alert-success" role="alert" style="background: #ffc629;margin: 0px 0px 1px;"><span style="text-align: center;color: rgb(0,0,0);font-size: 18px;font-family: 'Quattrocento Sans', sans-serif;"><i class="far fa-bell"></i><strong>Alerts: </strong><a class="link" href="#" style="color: rgb(0,0,0);font-family: 'Quattrocento Sans', sans-serif;"> Covid-19 | </a><a class="link" href="#" style="color: rgb(0,0,0);font-family: 'Quattrocento Sans', sans-serif;">Return to Campus|</a><a class="link" href="#" style="color: rgb(0,0,0);font-family: 'Quattrocento Sans', sans-serif;"> CARES Act</a></span></div>
     </div>
 
     <nav class="navbar navbar-lit navbar-expand-md navigation-clean-button" style="background: #0c1975;">
@@ -15,13 +19,13 @@
 
                 <ul class="nav navbar-nav mr-auto">
 
-                    <li class="nav-item"><a class="nav-link active" href="https://www.csub.edu/sustainability/foodpantry/FAQ_FoodPantry/Food%20Pantry%20Donation%20Suggestions.pdf" style="color: rgb(255,255,255);font-family: 'Roboto Slab', serif;">Donations</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="https://www.csub.edu/sustainability/foodpantry/FAQ_FoodPantry/Food%20Pantry%20Donation%20Suggestions.pdf" style="color: rgb(255,255,255);font-family: 'Roboto Slab', serif;"><strong>Donations</strong></a></li>
 
-                    <li class="nav-item"><a class="nav-link" href="#" style="color: rgb(255,255,255);font-family: 'Roboto Slab', serif;">Inventory</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#" style="color: rgb(255,255,255);font-family: 'Roboto Slab', serif;"><strong>Inventory</strong></a></li>
 
-                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#" style="color: rgb(255,255,255);font-family: 'Roboto Slab', serif;">Contact</a>
+                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#" style="color: rgb(255,255,255);font-family: 'Roboto Slab', serif;"><strong>Contact</strong></a>
 
-                        <div class="dropdown-menu"><a class="dropdown-item" href="#" style="color: rgb(255,255,255);font-family: 'Roboto Slab', serif;">Phone number</a><a class="dropdown-item" href="#">Location</a></div>
+                        <div class="dropdown-menu"><a class="dropdown-item" href="#" style="color: rgb(255,255,255);font-family: 'Roboto Slab', serif;"><strong>Phone number</strong></a><a class="dropdown-item" href="#"><strong>Location</strong></a></div>
                     </li>
 
                 </ul>
