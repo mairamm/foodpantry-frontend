60fc502a8d3fe372473ee243be6e495c5ae2a069
Maira Mendez <mmendez18@csub.edu>
2021-03-18 05:16:56 -0700

    fixed background color on homepage

--- restrucured/Homepage.php
+++ restrucured/Homepage.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 
+<div style="background-image: url('https://3.bp.blogspot.com/-gtWwwBppZyU/XBLm3TuUrGI/AAAAAAAABX8/fkFVUacEpakT2T6bvZKDzN0bNvsiEdfbQCHMYCw/s1600/1280x1024-light-gray-solid-color-background.jpg');">
+
 <head>
 <?php
     include('skel/header.php');
@@ -9,18 +11,18 @@
     <link rel="stylesheet" href="./assets/css/lib/bootstrap.min.css">
 </head>
 
-
 <section class="page-section clearfix">
     <div class="container">
         <div class="intro"><img class="img-fluid intro-img mb-3 mb-lg-0 rounded" src="./assets/img/comeSign.jpg">
             <div class="text-center intro-text p-5 rounded bg-faded">
                 <h2 class="section-heading mb-4"><span class="section-heading-upper">About Us</span><span class="section-heading-lower">Food Pantry</span></h2>
-                <p class="mb-3"><p style="font-size: 16px;">The CSUB Food Pantry is open to CSUB students, staff, and faculty. 
+                <p class="mb-3"><p style="font-size: 16px;">The CSUB Food Pantry is open to CSUB students, staff, and faculty.
                 <br>The Food Pantry is guided by the CSUB Food and Housing Security Committee, the Sustainability Department, and the Executive Director of Associated Students, Inc. The project goals are to increase food security, reduce food waste, and expand nutritional offerings at CSU Bakersfield.
                 <br>Due to the temporary closure of the CSUB campus, the CSUB Food Pantry will have reduced hours of operation beginning March 18th, 2020.</p></p>
         </div>
     </div>
 </section>
+
 
 
 <?php
