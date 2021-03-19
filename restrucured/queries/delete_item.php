0db6617256dcef9d3c553a4e83217e379bb33a59
Randy690 <mangattrandy017@gmail.com>
2021-03-19 11:18:28 -0700

    made some changes to delete

--- restrucured/queries/delete_item.php
+++ restrucured/queries/delete_item.php
@@ -6,10 +6,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$prodid = mysqli_real_escape_string($conn, $_POST['prod-id']);
-$brandid = mysqli_real_escape_string($conn, $_POST['brand-id']);
+//$prodid = mysqli_real_escape_string($conn, $_POST['prod-id']);
+//$brandid = mysqli_real_escape_string($conn, $_POST['brand-id']);
 
-$sql1 = "DELETE FROM inventory WHERE productID LIKE '%" . $prodid . "%'";
+$sql1 = "DELETE FROM inventory WHERE prod-id = 8";
 $sql2 = "DELETE FROM `brand-type` WHERE brandID LIKE '%" . $brandid . "%'";
 
 if(mysqli_query($conn, $sql1) && mysqli_query($conn, $sql2))
