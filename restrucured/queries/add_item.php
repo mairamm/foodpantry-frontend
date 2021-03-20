5cfdff0282b812c5962b5cdc7414d8d539ea0cf5
ArieBattle <abattle@csub.edu>
2021-03-20 10:24:27 -0700

    fixed the add_item.php

--- restrucured/queries/add_item.php
+++ restrucured/queries/add_item.php
@@ -1,6 +1,4 @@
 <?php
-
-echo "made it here";
 //include handles connection to database
 include ('dbh.inc.php');
 //displays errors and warnings
@@ -9,13 +7,13 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
- $prodid mysqli_real_escape_string($conn, $_POST['Prod_ID']);
- $brandid mysqli_real_escape_string($conn, $_POST['Brand_ID']);
- $quantity mysqli_real_escape_string($conn, $_POST['quantity']);
- $pointcost mysqli_real_escape_string($conn, $_POST['Point_cost']);
- $expirationdate mysqli_real_escape_string($conn, $_POST['expiration_date']);
- $brandname mysqli_real_escape_string($conn, $_POST['Brand_Name']);
- $producetype mysqli_real_escape_string($conn, $_POST['Produce_type']);
+ $prodid = mysqli_real_escape_string($conn, $_POST['Prod_ID']);
+ $brandid = mysqli_real_escape_string($conn, $_POST['Brand_ID']);
+ $quantity = mysqli_real_escape_string($conn, $_POST['quantity']);
+ $pointcost = mysqli_real_escape_string($conn, $_POST['Point_cost']);
+ $expirationdate = mysqli_real_escape_string($conn, $_POST['expiration_date']);
+ $brandname = mysqli_real_escape_string($conn, $_POST['Brand_Name']);
+ $producetype = mysqli_real_escape_string($conn, $_POST['Produce_type']);
 
 //will insert data from the user into the inventory 
  $sql = "INSERT INTO inventory(`prod-id`, `brand-id`, quantity, `point-cost`, `expiration-date`, `brand-name`, `produce-type`) VALUES('" . $prodid . "','" . $brandid . "','" . $quantity . "','" . $pointcost . "','" . $expirationdate . "','" $brandname . "','" $producetype ."',0 )";
