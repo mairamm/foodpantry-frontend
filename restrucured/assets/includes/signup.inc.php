eb97322bc459ee595b5865f67f6bdc738289da60
Loay Samha <lsamha@csub.edu>
2021-03-24 09:48:19 -0700

    Modified signup.php to connect with signup.inc.php and modfied signup.inc.php

--- restrucured/assets/includes/signup.inc.php
+++ restrucured/assets/includes/signup.inc.php
@@ -5,7 +5,7 @@
 # This page also include db connctions include page & functions include page
 
 # Checking if a submit post request is initiated
-if (isset($_POST["submit"])) {
+if (isset($_POST["signupUserSubmit"])) {
   $fname = $_POST["fname"];
   $lname = $_POST["lname"];
   $email = $_POST["email"];
@@ -20,37 +20,37 @@ if (isset($_POST["submit"])) {
   # ERROR HANDLERS
   #If the form is empty
   if (signupEmpty($fname, $lname, $email, $passwd, $passwdRepeat) == true ) {
-    header("location: ../signup.php?error=emptyform");
+    header("location: ../../signup.php?error=emptyform");
     exit();
   }
 
   #If the fname sysntax is wrong. Sanitize using regular experssions
   if (badSyntaxFname($fname) == true ) {
-    header("location: ../signup.php?error=badsyntaxfname");
+    header("location: ../../signup.php?error=badsyntaxfname");
     exit();
   }
 
   #If the fname sysntax is wrong. Sanitize using regular experssions
   if (badSyntaxLname($lname) == true ) {
-    header("location: ../signup.php?error=badsyntaxlname");
+    header("location: ../../signup.php?error=badsyntaxlname");
     exit();
   }
 
   #If email is not in the right format
   if (invalidEmail($email) == true ) {
-    header("location: ../signup.php?error=invalidemail");
+    header("location: ../../signup.php?error=invalidemail");
     exit();
   }
 
   #If the passwords dont match
   if (passwdMatching($passwd, $passwdRepeat) == true ) {
-    header("location: ../signup.php?error=passwordsdontmatch");
+    header("location: ../../signup.php?error=passwordsdontmatch");
     exit();
   }
 
   #If you the email is taken by another user.
   if (emailTaken($connection, $email) == true ) {
-    header("location: ../signup.php?error=emailalreadyexists");
+    header("location: ../../signup.php?error=emailalreadyexists");
     exit();
   }
 
@@ -58,6 +58,5 @@ if (isset($_POST["submit"])) {
  signupUser($connection, $fname, $lname, $email, $passwd);
 
 } else {
-  header("location: ../signup.php");
+  header("location: ../../signup.php");
 }
-
--- restrucured/signup.php
+++ restrucured/signup.php
@@ -1,45 +1,65 @@
-<!DOCTYPE html>
-<html>
-
 <?php
+session_start();
+#if (!isset($_SESSION['QRcode'])) {
+#  echo '<h1>ERROR 404 PAGE NOT FOUND</h1>';
+#} else {}
     include('skel/header.php');
     include('skel/navbarReg.php');
+#====================================BODY=======================================
 ?>
-
 <body>
     <main class="page lanidng-page"></main>
     <section class="portfolio-block website gradient" style="background: url(&quot;./assets/img/gray.png&quot;);">
-        <section>
             <h1 class="text-center text-capitalize" style="font-size: 20px;color: #0c1975;"><br>Create your account by filling the form below.<br></h1>
             <div class="container">
                 <form id="application-form" style="background: url(&quot;./assets/img/white.jpg&quot;);color: #0c1975;">
                     <div class="form-group">
                         <div class="form-row">
                             <div class="col">
-                                <p><strong>First Name</strong>&nbsp;<span class="text-danger">*</span></p><input class="form-control" type="text" required="" name="">
+                                <p><strong>First Name</strong>&nbsp;<span class="text-danger">*</span></p><input class="form-control" type="text" required="" name="fname">
                             </div>
                             <div class="col">
-                                <p><strong>Last Name</strong>&nbsp;<span class="text-danger">*</span></p><input class="form-control" type="text" required="" name="">
+                                <p><strong>Last Name</strong>&nbsp;<span class="text-danger">*</span></p><input class="form-control" type="text" required="" name="lname">
                             </div>
                         </div>
                     </div>
                     <div class="form-group">
-                        <p><strong>Email&nbsp;</strong><span class="text-danger">*</span></p><input class="form-control" type="email" name="" placeholder="rowdy@csub.edu">
+                        <p><strong>Email&nbsp;</strong><span class="text-danger">*</span></p><input class="form-control" type="email" name="email" placeholder="rowdy@csub.edu">
                     </div>
                     <div class="form-group">
-                        <p><strong>Password&nbsp;</strong><span class="text-danger">*</span></p><input class="form-control" type="text" required="" name="">
+                        <p><strong>Password&nbsp;</strong><span class="text-danger">*</span></p><input class="form-control" type="text" required="" name="passwd">
                     </div>
                     <div class="form-group">
-                        <p><strong>Verify Password&nbsp;</strong><span class="text-danger">*</span></p><input class="form-control" type="text" required="" name="">
+                        <p><strong>Verify Password&nbsp;</strong><span class="text-danger">*</span></p><input class="form-control" type="text" required="" name="passwdRepeat">
                     </div>
 
                 <br>
-                <span class="submit"><a class="submit" role="button" href="#" style="background: #ffc629;color: rgb(0,0,0);padding: 8px 20px;">Sign me up!</a></span>
+                <span class="submit"><a class="submit" role="button" name="signupUserSubmit" href="#" style="background: #ffc629;color: rgb(0,0,0);padding: 8px 20px;">Sign me up!</a></span>
                 <br>
-<br>
+              <br>
                 </form>
             </div>
-        </section>
+            <?php
+                  if(isset($_GET["error"])) {
+                    if ($_GET["error"] == "emptyform") {
+                      echo "<p>Fill in the fields</p>";
+                    }elseif ($_GET["error"] == "badsyntaxfname") {
+                      echo "<p>Email format is wrong!</p>";
+                    }elseif ($_GET["error"] == "badsyntaxlname") {
+                      echo "<p>Email format is wrong!</p>";
+                    }elseif ($_GET["error"] == "invalidemail") {
+                      echo "<p>Email format is wrong!</p>";
+                    }elseif ($_GET["error"] == "passwordsdontmatch") {
+                      echo "<p>OH MY GOD the password is doesnt match. do it again...</p>";
+                    }elseif ($_GET["error"] == "emailalreadyexists") {
+                      echo "<p>Email alread exists in the databases! hmmm...OO </p>";
+                    }elseif ($_GET["error"] == "sqlfail") {
+                      echo "<p>statment failed lol</p>";
+                    }elseif ($_GET["error"] == "noerror") {
+                      echo "<p>Employee has been registered! Cool! what is next? </p>";
+                    }
+                  }
+                 ?>
     </section>
     <script src="./assets/js/jquery.min.js"></script>
     <script src="./assets/js/bootstrap.min.js"></script>
@@ -51,6 +71,7 @@
 </body>
 
 <?php
+#=================================END-BODY======================================
     include('skel/footer.php');
 ?>
 
