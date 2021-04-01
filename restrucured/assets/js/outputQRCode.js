b0e2d4bf2aede2737e3348443f7a3c74ba3fcdc1
alo-beep <lopeza1829@gmail.com>
2021-03-31 21:12:16 -0700

    QRCode generated now redirects to signup page with unique QRcodeID in header (currently hardcoded). Also, randStringGenerator.js was removed as it had to be rewritten in php

--- restrucured/assets/js/outputQRCode.js
+++ restrucured/assets/js/outputQRCode.js
@@ -3,7 +3,14 @@
 document.write('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js" ></script>');
 
 function outputQRCodetoID(id, data) {
-  console.log(id);
-  console.log(data);
-  var qrc = new QRCode(document.getElementById(id), data);
+  var link = "174.135.102.61/project/foodpantryTEST/"
+    + "foodpantry-frontend/restrucured/signup.php?qrcode="
+    + data;
+  //var link = "google.com";
+  var qrc = new QRCode(document.getElementById(id), {
+  text: link,
+  width: 350,
+  height: 350,
+  correctLevel: QRCode.CorrectLevel.L
+  });
 }
--- restrucured/staff/staffRegister.php
+++ restrucured/staff/staffRegister.php
@@ -7,7 +7,6 @@ error_reporting(E_ALL);
 ?>
 
 <script src="../assets/js/outputQRCode.js"></script>
-<script src="../assets/js/randStringGenerator.js"></script>
 
 <body>
   <div id="qrcode"></div>
@@ -21,8 +20,8 @@ error_reporting(E_ALL);
     // generate random string
     $qrcodeid = generateRandString();
     // check if string generated is unique for QRcode ID
+    //  if it is, output the QRCode to the screen
     if (qrCodeIDIsUnique($qrcodeid)) {
-      // if it is, output the QRCode to the screen
       ?>
       <script type="text/javascript">
         window.onload = function() {
