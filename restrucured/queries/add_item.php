09bf17c7bd5ac8f942187fbe16cfa3248c8e8b9e
Randy690 <mangattrandy017@gmail.com>
2021-04-01 13:08:35 -0700

    chagnes to add item once again

--- restrucured/queries/add_item.php
+++ restrucured/queries/add_item.php
@@ -7,13 +7,13 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
- $prodid = mysqli_real_escape_string($connection, $_POST['Prod_ID']);
- $brandid = mysqli_real_escape_string($connection, $_POST['Brand_ID']);
+ $prodid = mysqli_real_escape_string($connection, $_POST['prod-id']);
+ $brandid = mysqli_real_escape_string($connection, $_POST['brand-id']);
  $quantity = mysqli_real_escape_string($connection, $_POST['quantity']);
- $pointcost = mysqli_real_escape_string($connection, $_POST['Point_cost']);
- $expirationdate = mysqli_real_escape_string($connection, $_POST['expiration_date']);
- $brandname = mysqli_real_escape_string($connection, $_POST['Brand_Name']);
- $producetype = mysqli_real_escape_string($connection, $_POST['Produce_type']);
+ $pointcost = mysqli_real_escape_string($connection, $_POST['point-cost']);
+ $expirationdate = mysqli_real_escape_string($connection, $_POST['expiration-date']);
+ $brandname = mysqli_real_escape_string($connection, $_POST['brand-name']);
+ $producetype = mysqli_real_escape_string($connection, $_POST['produce-type']);
 
 //will insert data from the user into the inventory 
  $sql = "INSERT INTO inventory(`prod-id`, `brand-id`, quantity, `point-cost`, `expiration-date`, `brand-name`, `produce-type`) VALUES('" . $prodid . "','" . $brandid . "','" . $quantity . "','" . $pointcost . "','" . $expirationdate . "','" $brandname . "','" $producetype ."',0 )";
