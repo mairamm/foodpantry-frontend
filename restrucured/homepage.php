d060371135535d642234f41c3ea954cde23fa212
Maira Mendez <mmendez18@csub.edu>
2021-04-01 01:12:09 -0700

    fixed what broke after we moved the project

--- restrucured/homepage.php
+++ restrucured/homepage.php
@@ -11,50 +11,6 @@
     <link rel="stylesheet" href="./assets/css/lib/bootstrap.min.css">
 </head>
 
-<<<<<<< HEAD
-<body style="color: rgb(0,0,0);font-size: 15px;text-align: center;">
-    <div>
-        <div class="alert alert-success" role="alert" style="background: #ffc629;margin: 0px 0px 1px;"><span style="text-align: center;color: rgb(0,0,0);font-size: 20px;"><strong>Alerts:</strong><a class="alert-link" href="#" style="color: rgb(0,0,0);">Covid-19<strong>|</strong></a><a class="alert-link" href="#" style="color: rgb(0,0,0);">Return to Campus<strong>|</strong></a><a class="alert-link" href="#" style="color: rgb(0,0,0);">CARES Act</a></span></div>
-    </div>
-    <nav class="navbar navbar-lit navbar-expand-md navigation-clean-button" style="background: #0c1975;">
-        <div class="container"><a class="navbar-brand" href="#" style="color: rgb(255,255,255);font-size: 35px;font-style: normal;font-family: Bitter, serif;">Food Pantry</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navcol-1">
-                <ul class="nav navbar-nav mr-auto">
-                    <li class="nav-item"><a class="nav-link active" href="#" style="color: rgb(255,255,255);">Donations</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#" style="color: rgb(255,255,255);">Inventory</a></li>
-                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#" style="color: rgb(255,255,255);">Contact</a>
-                        <div class="dropdown-menu"><a class="dropdown-item" href="#">Phone number</a><a class="dropdown-item" href="#">Location</a></div>
-                    </li>
-                </ul><span class="navbar-text actions"><a class="btn btn-light action-button" role="button" href="#" style="background: #ffc629;color: rgb(0,0,0);padding: 8px 20px;">Login</a><a class="btn btn-light action-button" role="button" href="http://delphi.cs.csubak.edu/~abattle/foodpantry-frontend/restrucured/registration.php" style="background: #ffc629;color: rgb(0,0,0);">Sign Up</a></span>
-            </div>
-        </div>
-    </nav>
-    <div class="simple-slider">
-        <div class="swiper-container">
-            <div class="swiper-wrapper"></div>
-            <div class="swiper-pagination"></div>
-            <div class="swiper-button-prev"></div>
-            <div class="swiper-button-next"></div>
-        </div>
-        <div class="carousel slide carousel-fade" data-ride="carousel" data-interval="1000" id="carousel-1">
-            <div class="carousel-inner">
-                <div class="carousel-item active"><img class="w-100 d-block" src="./assets/img/manSpeaking.jpg" alt="Slide Image" width="800" height="533" /></div>
-                <div class="carousel-item">
-                    <p class="lead"><br />&quot;CSUB Food Pantry &amp; Edible Garden: A holistic Approach to Fighting Campus Food Insecurities&quot;<br /><br /></p><img class="w-100 d-block" src="./assets/img/EdibleGardenSignandRaisedBed.jpg" alt="Slide Image" width="800" height="300" />
-                </div>
-                <div class="carousel-item"><img class="w-100 d-block" src="./assets/img/Food pantry map.png" alt="Slide Image" /></div>
-                <div class="carousel-item"><img class="w-100 d-block" src="./assets/img/stockedShelf.jpg" alt="Slide Image" /></div>
-                <div class="carousel-item"><img class="w-100 d-block" src="./assets/img/distribution.webp" alt="Slide Image" /></div>
-            </div>
-            <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-slide="prev"><span class="carousel-control-prev-icon"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-slide="next"><span class="carousel-control-next-icon"></span><span class="sr-only">Next</span></a></div>
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-1" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-1" data-slide-to="1"></li>
-                <li dta-target="#carousel-1" data-slide-to="2"></li>
-                <li data-target="#carousel-1" data-slide-to="3"></li>
-                <li data-target="#carousel-1" data-slide-to="4"></li>
-            </ol>
-=======
 <section class="page-section clearfix">
     <div class="container">
         <div class="intro"><img class="img-fluid intro-img mb-3 mb-lg-0 rounded" src="./assets/img/comeSign.jpg">
@@ -63,7 +19,6 @@
                 <p class="mb-3"><p style="font-size: 16px;">The CSUB Food Pantry is open to CSUB students, staff, and faculty.
                 <br>The Food Pantry is guided by the CSUB Food and Housing Security Committee, the Sustainability Department, and the Executive Director of Associated Students, Inc. The project goals are to increase food security, reduce food waste, and expand nutritional offerings at CSU Bakersfield.
                 <br>Due to the temporary closure of the CSUB campus, the CSUB Food Pantry will have reduced hours of operation beginning March 18th, 2020.</p></p>
->>>>>>> fdd08cf01adb18c89bf9628008b30cd2c5d0b6b3
         </div>
     </div>
 </section>
