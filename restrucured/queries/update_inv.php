2432c1cd7c57578ab0c727fb85db9efaa36378c3
Randy690 <mangattrandy017@gmail.com>
2021-04-01 11:47:49 -0700

    made some changes to the update_inv.php file

--- restrucured/queries/update_inv.php
+++ restrucured/queries/update_inv.php
@@ -1,21 +1,19 @@
 <?php
-include 'dbh.inc.php';
+include ('dbh.inc.php');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
- $prodid mysqli_real_escape_string($connection, $_POST['Prod_ID']);
- $brandid mysqli_real_escape_string($connection, $_POST['Brand_ID']);
+ $prodid mysqli_real_escape_string($connection, $_POST['prod-id']);
+ $brandid mysqli_real_escape_string($connection, $_POST['brand-id']);
  $quantity mysqli_real_escape_string($connection, $_POST['quantity']);
- $pointcost mysqli_real_escape_string($connection, $_POST['Point_cost']);
- $expirationdate mysqli_real_escape_string($connection, $_POST['expiration_date']);
- $brandname mysqli_real_escape_string($connection, $_POST['Brand_Name']);
- $producetype mysqli_real_escape_string($connection, $_POST['Produce_type']);
+ $pointcost mysqli_real_escape_string($connection, $_POST['point-cost']);
+ $expirationdate mysqli_real_escape_string($connection, $_POST['expiration-date']);
+ $brandname mysqli_real_escape_string($connection, $_POST['brand-name']);
+ $producetype mysqli_real_escape_string($connection, $_POST['produce-type']);
  
  if(mysqli_query($connection, "CALL upInventory(". $prodid . "," . $brandid . ",'" . $quantity . "'," . $pointcost . "," . $brandname . "," . $producetype . ");")){
-	 
-	 
-	echo "Inventory updated.";
+ echo "Inventory updated.";
 } else{
     echo "ERROR: Could not update $sql. " . mysqli_error($connection);
 }
