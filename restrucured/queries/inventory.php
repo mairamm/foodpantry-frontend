d2f325af6d9aae19192f9d639ca6fc2d9262084f
Randy690 <mangattrandy017@gmail.com>
2021-04-01 11:02:15 -0700

    Made changes to staff.php, will show the inventory now

--- restrucured/queries/inventory.php
+++ restrucured/queries/inventory.php
@@ -10,7 +10,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$query = "SELECT i.`prod-id`, b.`brand-name`, b.`produce-type`, i.quantity, i.`point-cost`, i.`expiration-date` FROM `brand-type` AS b INNER JOIN inventory AS i ON i.`brand-id` = b.`brand-id`;";
+$query = "SELECT i.`prod-id`, b.`brand-name`, b.`produce-type`, i.quantity, i.`point-cost`, i.`expiration-date` FROM `brandType` AS b INNER JOIN inventory AS i ON i.`brand-id` = b.`brand-id`;";
 
 
 if($result = mysqli_query($connection, $query))
