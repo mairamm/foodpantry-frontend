54c8b787e18cab6832abc53944823bd0446a8b4a
Maira Mendez <mmendez18@csub.edu>
2021-04-02 00:35:43 -0700

    temporary fix for the log in and sign up buttons on homepage

--- restrucured/skel/navbarHome.php
+++ restrucured/skel/navbarHome.php
@@ -30,7 +30,7 @@
 
                 </ul>
             </div>
-    <span class="navbar-text actions"><a class="btn btn-primary" role="button" href="#" style="background: #ffc629;color: rgb(12, 25, 117);font-family: 'Roboto Slab', serif;float: right;">Login</a><a class="btn btn-primary" role="button" href="http://delphi.cs.csubak.edu/~foodpantry2/foodpantry-frontend/frontEnd/register/" style="background: #ffc629;color: rgb(12, 25, 117);font-family: 'Roboto Slab', serif;float: right;">Sign Up</a></span>
+    <span class="navbar-text actions"><a class="btn btn-primary" role="button" href="http://174.135.102.61/project/foodpantry-frontend/restrucured/login.php" style="background: #ffc629;color: rgb(12, 25, 117);font-family: 'Roboto Slab', serif;float: right;">Login</a><a class="btn btn-primary" role="button" href="http://174.135.102.61/project/foodpantry-frontend/restrucured/signup.php" style="background: #ffc629;color: rgb(12, 25, 117);font-family: 'Roboto Slab', serif;float: right;">Sign Up</a></span>
         </div>
     </nav>
 </body>
