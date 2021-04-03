87b1b40c950d554406ca671225e37cd1d5bced1a
Maira Mendez <mmendez18@csub.edu>
2021-04-03 01:18:19 -0700

    fixed the signup button on the login page

--- restrucured/skel/navbarLogin.php
+++ restrucured/skel/navbarLogin.php
@@ -13,7 +13,7 @@
 
 
     <nav class="navbar navbar-lit navbar-expand-md navigation-clean-button" style="background: #0c1975;">
-        <div class="container"><a class="navbar-brand" href="#" style="color: rgb(255,255,255);font-size: 35px;font-style: normal;font-family: 'Roboto Slab', serif;">Food Pantry</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="container"><a class="navbar-brand" href="http://174.135.102.61/project/foodpantry-frontend/restrucured/homepage.php" style="color: rgb(255,255,255);font-size: 35px;font-style: normal;font-family: 'Roboto Slab', serif;">Food Pantry</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navcol-1">
                 <ul class="nav navbar-nav mr-auto">
                     <li class="nav-item"><a class="nav-link active" href="https://www.csub.edu/sustainability/foodpantry/FAQ_FoodPantry/Food%20Pantry%20Donation%20Suggestions.pdf" style="color: rgb(255,255,255);font-family: 'Roboto Slab', serif;"><strong>Donations</strong></a></li>
@@ -23,7 +23,7 @@
                     </li>
                 </ul>
             </div>
-    <span class="navbar-text actions">Login</a><a class="btn btn-primary" role="button" href="http://delphi.cs.csubak.edu/~foodpantry2/foodpantry-frontend/frontEnd/register/" style="background: #ffc629;color: rgb(12, 25, 117);font-family: 'Roboto Slab', serif;float: right;">Sign Up</a></span>
+    <a class="btn btn-primary" role="button" href="http://174.135.102.61/project/foodpantry-frontend/restrucured/signup.php" style="background: #ffc629;color: rgb(12, 25, 117);font-family: 'Roboto Slab', serif;float: right;">Sign Up</a></span>
         </div>
     </nav>
 </body>
