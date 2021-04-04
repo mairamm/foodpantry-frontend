64db6ccbf2441a263ebc5e7a7c93240aafefcb4a
Maira Mendez <mmendez18@csub.edu>
2021-04-04 05:26:54 -0700

    added the Our Location section to the home page

--- restrucured/homepage.php
+++ restrucured/homepage.php
@@ -28,15 +28,57 @@
     <div class="product-item">
         <div class="d-flex product-item-title">
             <div class="d-flex mr-auto bg-faded p-5 rounded">
+                <img class="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="./assets/img/stockedShelf.jpg" style="width: 500px; padding-right: 20px;" />
                 <h2 class="section-heading mb-0"><span class="section-heading-lower">How does it work? </span>
                 <span class="section-heading-upper">Each visitor is allotted one visit per week and up to 16 points per visit</span>
-                <img src="./assets/img/points.jpg"/>
+                <img src="./assets/img/pointsExample.jpg" style="width: 520px"/>
                 </h2>
-            <img class="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="./assets/img/stockedShelf.jpg" style="width: 500px;" />
             </div>
          </div>
     </div>
 </div>
+
+<br><br><br><br><br>
+
+<div class="container">
+    <div class="product-item">
+        <div class="d-flex product-item-title">
+            <div class="d-flex mr-auto bg-faded p-5 rounded">
+                <div>
+                <h2 class="section-heading mb-4">
+                <span class="section-heading-lower">Our Location </span></h2>
+                <p class="mb-3"><p style="font-size: 16px;">Hours of Operation During Closure:</p>
+                <table style="font-size: 15px;">
+                            <thead>
+                                <tr>
+                                    <th>Monday</th>
+                                    <th>Tuesday</th>
+                                    <th>Wednesday</th>
+                                    <th>Thursday</th>
+                                    <th>Friday</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>10 a.m. - 2 p.m.</td>
+                                    <td>2 p.m. - 6 p.m.</td>
+                                    <td>10 a.m. - 2 p.m.</td>
+                                    <td>2 p.m. - 6 p.m.</td>
+                                    <td>10 a.m. - 2 p.m.</td>
+                                </tr>
+                </table>
+
+                <p class="mb-3"><p style="font-size: 16px;">Volunteering:</p></p>
+                <p style="font-size: 16px;">Want to volunteer at the Food Pantry, Edible Garden or at any of our Sustainability Events, Click below to sign up!</p>
+                <a href="https://signup.com/go/xrLkksd"><img src="./assets/img/volunteerBtn.png"/></a>
+            </div>
+            <img class="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="./assets/img/Food pantry map.png" style="width: 500px;" />
+            </div>
+         </div>
+    </div>
+</div>
+
+
 
 <br><br><br>
 
