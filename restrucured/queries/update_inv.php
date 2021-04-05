5a71859be63fb7325363c8270fe1bbaa770776b2
ArieBattle <alejoarielle@gmail.com>
2021-04-05 11:28:30 -0700

    updated the update inventory php, made it so when you enter it takes you to a page instead of a blank page

--- restrucured/queries/update_inv.php
+++ restrucured/queries/update_inv.php
@@ -10,19 +10,34 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
- $prodid = mysqli_real_escape_string($connection, $_POST[`prod-id`]);
- $brandid = mysqli_real_escape_string($connection, $_POST[`brand-id`]);
- $quantity = mysqli_real_escape_string($connection, $_POST[`quantity`]);
- $pointcost = mysqli_real_escape_string($connection, $_POST[`point-cost`]);
- $expirationdate = mysqli_real_escape_string($connection, $_POST[`expiration-date`]);
- $brandname = mysqli_real_escape_string($connection, $_POST[`brand-name`]);
- $producetype = mysqli_real_escape_string($connection, $_POST[`produce-type`]);
- 
- if(mysqli_query($connection, "CALL upinventory(". $prodid . "," . $brandid . ",'" . $quantity . "'," . $pointcost . "," . $brandname . "," . $producetype . ");")){
- echo "Inventory updated.";
+$prodid = mysqli_real_escape_string($connection, $_POST[`prod-id`]);
+$brandid = mysqli_real_escape_string($connection, $_POST[`brand-id`]);
+$quantity = mysqli_real_escape_string($connection, $_POST[`quantity`]);
+$pointcost = mysqli_real_escape_string($connection, $_POST[`point-cost`]);
+$expirationdate = mysqli_real_escape_string($connection, $_POST[`expiration-date`]);
+$brandname = mysqli_real_escape_string($connection, $_POST[`brand-name`]);
+$producetype = mysqli_real_escape_string($connection, $_POST[`produce-type`]);
+
+if(mysqli_query($connection, "CALL upinventory(". $prodid . "," . $brandid . ",'" . $quantity . "'," . $pointcost . "," . $brandname . "," . $producetype . ");")){
+  echo "Inventory updated.";
 } else{
-    echo "ERROR: Could not update $sql. " . mysqli_error($connection);
+  echo "ERROR: Could not update $sql. " . mysqli_error($connection);
 }
 
 mysqli_close($connection);
 ?>
+
+<html>
+<body>
+<h1>GO BACK TO:</h1>
+
+<ul>
+<li>
+<a href="../homepage.php"><strong></strong>Home Page</a>
+</li>
+<li>
+<a href="../staff/staffUpdate.php"><strong>Update another</strong></a>
+</li>
+</ul>
+</body>
+</html>
