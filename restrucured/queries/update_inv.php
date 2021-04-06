1385d4e1a45f880f2c807ab8105abaed0d4be3f1
Randy690 <mangattrandy017@gmail.com>
2021-04-06 08:06:23 -0700

    Made some more changes to the update.php, it will take in onloy 4 fields now, still needs some debugging but it's almost there

--- restrucured/queries/update_inv.php
+++ restrucured/queries/update_inv.php
@@ -2,57 +2,39 @@
 include ('dbh.inc.php');
 
 //commented out temporarily due to pathing issue
-//require_once ('./assets/includes/update_item.inc.php')
+//require_once ('../assets/includes/update_item.inc.php')
 
 //displays all errors or warnings
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+$db = mysqli_connect("localhost","alopez","qWECGWGYh?fPByLxe@a", "foodpantry");
+
+//$prodid = mysqli_real_escape_string($connection, $_POST[`prod-id`]);
+//$brandid = mysqli_real_escape_string($connection, $_POST[`brand-id`]);
+//$quantity = mysqli_real_escape_string($connection, $_POST[`quantity`]);
+//$pointcost = mysqli_real_escape_string($connection, $_POST[`point-cost`]);
+//$expirationdate = mysqli_real_escape_string($connection, $_POST[`expiration-date`]);
+//$brandname = mysqli_real_escape_string($connection, $_POST[`brand-name`]);
+//$producetype = mysqli_real_escape_string($connection, $_POST[`produce-type`]);
+ 
+$statement = mysqli_prepare($db, "UPDATE inventory SET quantity=?, `point-cost`=?, `expiration-date`=? WHERE `prod-id`=?");
+
+if(!$statement){
+    echo "Prepare failed: (". $db->errno.") ".$db->error. "<br>";
+    }
+
+$statement->bind_param('ssss', $prodid, $quantity,$pointcost, $expirationdate);
 
 
-<<<<<<< HEAD
- $prodid = mysqli_real_escape_string($connection, $_POST[`prod-id`]);
- $brandid = mysqli_real_escape_string($connection, $_POST[`brand-id`]);
- $quantity = mysqli_real_escape_string($connection, $_POST[`quantity`]);
- $pointcost = mysqli_real_escape_string($connection, $_POST[`point-cost`]);
- $expirationdate = mysqli_real_escape_string($connection, $_POST[`expiration-date`]);
- $brandname = mysqli_real_escape_string($connection, $_POST[`brand-name`]);
- $producetype = mysqli_real_escape_string($connection, $_POST[`produce-type`]);
- 
- 
- 
- if(mysqli_query($connection, "CALL upinventory(". $prodid . "," . $brandid . ",'" . $quantity . "'," . $pointcost . "," . $brandname . "," . $producetype . ");")){
- echo "Inventory updated.";
-=======
-$prodid = mysqli_real_escape_string($connection, $_POST[`prod-id`]);
-$brandid = mysqli_real_escape_string($connection, $_POST[`brand-id`]);
-$quantity = mysqli_real_escape_string($connection, $_POST[`quantity`]);
-$pointcost = mysqli_real_escape_string($connection, $_POST[`point-cost`]);
-$expirationdate = mysqli_real_escape_string($connection, $_POST[`expiration-date`]);
-$brandname = mysqli_real_escape_string($connection, $_POST[`brand-name`]);
-$producetype = mysqli_real_escape_string($connection, $_POST[`produce-type`]);
-
-if(mysqli_query($connection, "CALL upinventory(". $prodid . "," . $brandid . ",'" . $quantity . "'," . $pointcost . "," . $brandname . "," . $producetype . ");")){
-  echo "Inventory updated.";
->>>>>>> 5a71859be63fb7325363c8270fe1bbaa770776b2
-} else{
-  echo "ERROR: Could not update $sql. " . mysqli_error($connection);
-}
+if ($statement->execute()) {
+    echo "Updated";
+    }
+    else {
+        echo "Error getting result " . mysqli_error($db);
+        die();
+    }
 
 mysqli_close($connection);
+
 ?>
-
-<html>
-<body>
-<h1>GO BACK TO:</h1>
-
-<ul>
-<li>
-<a href="../homepage.php"><strong></strong>Home Page</a>
-</li>
-<li>
-<a href="../staff/staffUpdate.php"><strong>Update another</strong></a>
-</li>
-</ul>
-</body>
-</html>
