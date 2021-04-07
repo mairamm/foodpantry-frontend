47a4bac27e41ee8c923425d70fd4457f373a26d5
Randy690 <mangattrandy017@gmail.com>
2021-04-07 11:42:50 -0700

    Removed some and commented out some unnecessary lines from the update_inv.php file and the staffAdd.php file

--- restrucured/queries/update_inv.php
+++ restrucured/queries/update_inv.php
@@ -10,14 +10,6 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $db = mysqli_connect("localhost","alopez","qWECGWGYh?fPByLxe@a", "foodpantry");
 
-//$prodid = mysqli_real_escape_string($connection, $_POST[`prod-id`]);
-//$brandid = mysqli_real_escape_string($connection, $_POST[`brand-id`]);
-//$quantity = mysqli_real_escape_string($connection, $_POST[`quantity`]);
-//$pointcost = mysqli_real_escape_string($connection, $_POST[`point-cost`]);
-//$expirationdate = mysqli_real_escape_string($connection, $_POST[`expiration-date`]);
-//$brandname = mysqli_real_escape_string($connection, $_POST[`brand-name`]);
-//$producetype = mysqli_real_escape_string($connection, $_POST[`produce-type`]);
- 
 $statement = mysqli_prepare($db, "UPDATE inventory SET quantity=?, `point-cost`=?, `expiration-date`=? WHERE `prod-id`=?");
 
 if(!$statement){
