4cb85d11b128289aa55931dbe26f3dbf79486a1c
Loay Samha <lsamha@csub.edu>
2021-04-08 14:40:27 -0700

    Debugging staff.php TRY00

--- restrucured/staff.php
+++ restrucured/staff.php
@@ -3,10 +3,14 @@
     include('skel/header.php');
     include('skel/navbar/staff.php');
     include('./assets/includes/staff/staffFunctions.inc.php');
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors',1);
+    error_reporting(E_ALL);
 #====================================BODY=======================================
 
-    include('./assets/includes/staff/inventoryC/view_inventory.inc.php');
+
     include('./assets/includes/staff/inventoryC/view_inventoryAndBrands.inc.php');
+    include('./assets/includes/staff/inventoryC/view_inventory.inc.php');
 
 
 #=================================END-BODY======================================
