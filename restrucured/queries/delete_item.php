619714c392344f149a330f171fcac185819b0f64
Randy690 <mangattrandy017@gmail.com>
2021-04-07 17:13:35 -0700

    BIG OVERHAUL, redid the delete and update queries as well as their respective staff pages, the submit button works and i have included 2 new include files in the queries folder. No errors with update but for some reason it is not working. The delete works perfectly fine nowgit add .

--- restrucured/queries/delete_item.php
+++ restrucured/queries/delete_item.php
@@ -1,35 +1,24 @@
 <?php
 
-//this include handles the connection to the database
-include ('../assets/includes/delete_item.inc.php');
-include ('dbh.inc.php');
-
 //the following 3 lines of code reports warnings and errors
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-//$prodid = mysqli_real_escape_string($connection, $_POST[`prod-id`]);
-//$brandid = mysqli_real_escape_string($conn, $_POST['brand-id']);
+if (isset($_POST["Submit"])) {
+    $prodid = $_POST["prod-id"];
 
-//$sql1 = "DELETE FROM inventory WHERE `prod-id` = '%" .$prodid . "'";
+//this include handles the connection to the database
+include ('deletefunc.inc.php');
+include ('dbh.inc.php');
 
-//$sql2 = "DELETE FROM `brand-type` WHERE brand-id LIKE '%" . $brandid . "%'";
 
-if(prodidEmpty($prodid) == true) {
+if(emptyField($prodid) == true) {
+    header("location: ../staff/staffDelete.php?error=EmptyID");
     echo "Empty field";
-    
     exit();
 }
 
-if(mysqli_query($connection, $sql1))
-    {
-            echo "Records deleted successfully.";
-    } 
-    else
-    {
-        echo "ERROR: Could not delete $sql1. " . mysqli_error($connection);
-    }
-    
-    mysqli_close($connection);
-    ?>
+deleteItem($connection, $prodid);
+}
+
--- restrucured/queries/update_inv.php
+++ restrucured/queries/update_inv.php
@@ -1,32 +1,20 @@
 <?php
-include ('dbh.inc.php');
-
-//commented out temporarily due to pathing issue
-//require_once ('../assets/includes/update_item.inc.php')
-
 //displays all errors or warnings
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-$db = mysqli_connect("localhost","alopez","qWECGWGYh?fPByLxe@a", "foodpantry");
 
-$statement = mysqli_prepare($db, "UPDATE inventory SET quantity=?, `point-cost`=?, `expiration-date`=? WHERE `prod-id`=?");
+if (isset($_POST["Submit"])) {
+    $prodid = $_POST["prod-id"];
+    $quantity = $_POST["quantity"];
+    $pointcost = $_POST["point-cost"];
+    $expirationdate = $_POST["expiration-date"];
 
-if(!$statement){
-    echo "Prepare failed: (". $db->errno.") ".$db->error. "<br>";
-    }
-
-$statement->bind_param('ssss', $prodid, $quantity,$pointcost, $expirationdate);
+    require_once ('dbh.inc.php');
+    require_once ('updatefunc.inc.php');
 
 
-if ($statement->execute()) {
-    echo "Updated";
-    }
-    else {
-        echo "Error getting result " . mysqli_error($db);
-        die();
-    }
+    updateInventory($connection, $prodid, $quantity, $pointcost, $expirationdate);
 
-mysqli_close($connection);
+}
 
-?>
