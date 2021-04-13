2c14d051a8be403862b79f8306692303490c5cf3
Randy690 <mangattrandy017@gmail.com>
2021-04-13 12:57:03 -0700

    fixed pathing on the delete item file, added functionality to the delete consumer.inc.php file

--- restrucured00/assets/includes/staff/consumerC/delete_consumer.inc.php
+++ restrucured00/assets/includes/staff/consumerC/delete_consumer.inc.php
@@ -1,26 +1,23 @@
 <?php
-//include will handle the connection to the database
-include '../../dbh.inc.php';
 
 //will display errors
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$QRcode = mysqli_real_escape_string($connection, $_POST['QRcode']);
+if (isset($_POST["Submit"])) {
+    $prodid = $_POST["QRcode"];
 
-//will delete the person from the database by their QRcode
-$sql1 = "DELETE FROM individual WHERE QRcode LIKE '%" . $QRcode . "%'";
-$sql2 = "DELETE FROM consumer WHERE QRcode LIKE '%" . $QRcode . "%'";
+include ('../../dbh.inc.php');
+include ('../staffFunctions.inc.php');
 
-if(mysqli_query($connection, $sql1) && mysqli_query($connection, $sql2))
-    {
-            echo "Records deleted successfully.";
-    }
-    else
-    {
-        echo "ERROR: Could not delete $sql. " . mysqli_error($connection);
-    }
 
-    mysqli_close($connection);
-    ?>
+if (emptyQRcode($QRcode) == true) {
+    header("location: ../../../../staff/consumerC/delete_consumer.php?error=EmptyQRcode");
+    echo "please enter a QRcode";
+    exit();
+}
+
+deleteIndividual($connection, $QRcode);
+
+}
