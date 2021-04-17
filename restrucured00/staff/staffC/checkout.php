67ff4bb1a6d74b985e5152b30677c83992f616d0
Maira Mendez <mmendez18@csub.edu>
2021-04-16 22:00:29 -0700

    added a check out button to restrucures00/staff.php that links to restrucured00/staff/staffC/checkout.php to check out the consumer's items

---
+++ restrucured00/staff/staffC/checkout.php
@@ -0,0 +1,10 @@
+<?php
+echo '<title> Staff Check Out - Foodpantry</title>';
+    include('../../skel/header.php');
+    include('../../skel/navbar/staff.php');
+    include('../../assets/includes/staff/staffFunctions.inc.php');
+
+#====================================BODY=======================================\
+?>
+
+
