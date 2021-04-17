1f8c264b17f5f6f9bb362b026ca06883c2a17868
alo-beep <lopeza1829@gmail.com>
2021-04-16 20:13:38 -0700

    fixed some links for the login page and started consumer QRCode output

--- restrucured00/consumer/view_qrcode.php
+++ restrucured00/consumer/view_qrcode.php
@@ -4,6 +4,17 @@ echo '<title> View Qrcode - Foodpantry</title>';
     include('../skel/navbar/consumer.php');
 
 #====================================BODY=======================================
+?>
+
+<script src="../assets/js/outputQRCode.js"></script>
+
+<body>
+  <div id="qrcode"></div>
+</body>
+<?php
+
+
+
 
 // This include below is just for you to include this link to the method next to the post request when trying to write this page
     include('../assets/includes/consumer/view_qrcode.inc.php');
