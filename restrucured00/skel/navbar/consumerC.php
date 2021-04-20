d5fb3887a5bf68d8e45ec0b60b325b3defd93d8f
Loay Samha <lsamha@csub.edu>
2021-04-20 14:10:58 -0700

    Fixed the paths links for the consumer controller page

--- restrucured00/skel/navbar/consumerC.php
+++ restrucured00/skel/navbar/consumerC.php
@@ -5,7 +5,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@310;400;700&display=swap" rel="stylesheet">
 
 <body style="color: rgb(0,0,0);font-size: 17px;">
-    
+
     <div class="alert alert-success" role="alert" style="background: #ffc629;margin: 0px 0px 1px;text-align: center;"><span style="text-align: center;color: rgb(0,0,0);font-size: 17px;font-family: 'Quattrocento Sans', sans-serif;"></i><a class="link" href="https://www.csub.edu/alert" style="color: rgb(0,0,0);font-family: 'Quattrocento Sans', sans-serif;"><strong>Alerts: </strong><a class="link" href="https://www.csub.edu/covid-19" style="color: rgb(0,0,0);font-family: 'Quattrocento Sans', sans-serif;"> Covid-19 | </a><a class="link" href="https://www.csub.edu/returntocampus" style="color: rgb(0,0,0);font-family: 'Quattrocento Sans', sans-serif;">Return to Campus |</a><a class="link" href="https://www.csub.edu/cares" style="color: rgb(0,0,0);font-family: 'Quattrocento Sans', sans-serif;"> CARES Act</a></span></div>
     </div>
 
@@ -14,9 +14,9 @@
 	<div class="container"><a class="navbar-brand" href="#" style="color: rgb(255,255,255);font-size: 35px;font-style: normal;font-family: 'Roboto Slab', serif;">Food Pantry</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
 	    <div class="collapse navbar-collapse" id="navcol-1">
 		<ul class="nav navbar-nav mr-auto">
-		    <li class="nav-item"><a class="nav-link active" href="#" style="color: rgb(255,255,255);font-family: 'Roboto Slab', serif;"><strong>ADD</strong></a></li>
- <li class="nav-item"><a class="nav-link" href="#" style="color: rgb(255,255,255);font-family: 'Roboto Slab', serif;"><strong>DELETE</strong></a></li>
-	  <li class="nav-item"><a class="nav-link" href="#" style="color: rgb(255,255,255);font-family: 'Roboto Slab', serif;"><strong>UPDATE</strong></a></li>
+		    <li class="nav-item"><a class="nav-link active" href="http://174.135.102.61/project/rmangat/foodpantry-frontend/restrucured00/staff/inventoryC/add_item.php" style="color: rgb(255,255,255);font-family: 'Roboto Slab', serif;"><strong>ADD</strong></a></li>
+	      <li class="nav-item"><a class="nav-link" href="http://174.135.102.61/project/rmangat/foodpantry-frontend/restrucured00/staff/inventoryC/update_item.php" style="color: rgb(255,255,255);font-family: 'Roboto Slab', serif;"><strong>UPDATE</strong></a></li>
+        <li class="nav-item"><a class="nav-link" href="http://174.135.102.61/project/rmangat/foodpantry-frontend/restrucured00/staff/inventoryC/delete_item.php" style="color: rgb(255,255,255);font-family: 'Roboto Slab', serif;"><strong>DELETE</strong></a></li>
 
 
 			<div class="dropdown-menu"><a class="dropdown-item" href="#" style="color: rgb(255,255,255);font-family: 'Roboto Slab', serif;"><strong>661-654-FOOD</strong></a><a class="dropdown-item" href="#"><strong>foodpantry@csub.edu</strong></a></div>
