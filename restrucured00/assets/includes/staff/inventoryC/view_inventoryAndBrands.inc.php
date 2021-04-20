3ea4dfb8ee3365d9c891b7d2a8c282b83e1b3adb
Randy690 <mangattrandy017@gmail.com>
2021-04-20 13:57:32 -0700

    Added brand id to staff invenvtory page

--- restrucured00/assets/includes/staff/inventoryC/view_inventoryAndBrands.inc.php
+++ restrucured00/assets/includes/staff/inventoryC/view_inventoryAndBrands.inc.php
@@ -13,7 +13,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$query = "SELECT i.`prod-id`, b.`brand-name`, b.`brand-id`, b.`produce-type`, i.quantity, i.`point-cost`, i.`expiration-date` FROM `brandType` AS b INNER JOIN inventory AS i ON i.`brand-id` = b.`brand-id`;";
+$query = "SELECT i.`prod-id`, b.`brand-id`, b.`brand-name`, b.`produce-type`, i.quantity, i.`point-cost`, i.`expiration-date` FROM `brandType` AS b INNER JOIN inventory AS i ON i.`brand-id` = b.`brand-id`;";
 
 
 if($result = mysqli_query($connection, $query))
@@ -23,6 +23,7 @@ if($result = mysqli_query($connection, $query))
 	echo "<table border='1';'>";
 	echo"<tr>";
 	echo "<th>ProductID</th>";
+	echo "<th>BrandID</th>";
 	echo "<th>Brand name</th>";
 	echo "<th>Product</th>";
 	echo "<th>Quantity</th>";
